3d76fe8bb7f15a07890ff9847f2bf9856dbf25b9
slimsky1 <40033936+slimsky1@users.noreply.github.com>
2020-03-17 17:24:51 -0300

    init commit

---
+++ Ejercicio2/Ejercicio2/Camino.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio2
+{
+    class Camino
+    {
+        private int numeroHoja;
+
+        private String tipoDeObjeto;
+
+        private int identificadorTramo;
+
+        private String tipoCamino;
+
+        private int longitud;
+        
+        public Camino(int numeroHoja, String tipoDeObjeto, int identificadorTramo, String tipoCamino, int longitud)
+        {
+            this.numeroHoja = numeroHoja;
+
+            this.tipoDeObjeto = tipoDeObjeto;
+
+            this.identificadorTramo = identificadorTramo;
+
+            this.tipoCamino = tipoCamino;
+
+            this.longitud = longitud;
+        }
+
+        public int NumeroHoja { get => numeroHoja; set => numeroHoja = value; }
+        public string TipoDeObjeto { get => tipoDeObjeto; set => tipoDeObjeto = value; }
+        public int IdentificadorTramo { get => identificadorTramo; set => identificadorTramo = value; }
+        public string TipoCamino { get => tipoCamino; set => tipoCamino = value; }
+        public int Longitud { get => longitud; set => longitud = value; }
+
+        public static void imprimir(List<Camino> listCaminos)
+        {
+            foreach (Camino camino in listCaminos)
+            {
+                Console.WriteLine(camino);
+            }
+            Console.ReadLine();
+        }
+
+        public override string ToString()
+        {
+            return this.numeroHoja + " " + this.tipoDeObjeto + " " + this.identificadorTramo + " " + this.tipoCamino + " " + this.longitud;
+        }
+    }
+}
---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(@"F:\Programacion\Visual Studio C#\Repos\Manejo_CSV\vial.csv"))
+            {
+
+                List<int> numerosHoja = new List<int>();
+
+                List<string> tiposDeObjeto = new List<string>();
+
+                List<int> identificadoresTramo = new List<int>();
+
+                List<string> tiposCamino = new List<string>();
+
+                List<int> longitud = new List<int>();
+
+                while (!reader.EndOfStream)
+                {
+
+                    var line = reader.ReadLine();
+
+                    var values = line.Split(',');
+
+                    numerosHoja.Add(int.Parse(values[0]));
+
+                    tiposDeObjeto.Add(values[1]);
+
+                    identificadoresTramo.Add(int.Parse(values[2]));
+
+                    tiposCamino.Add(values[3]);
+
+                    longitud.Add(int.Parse(values[4]));
+
+                }
+
+                List<Camino> listCaminos = new List<Camino>();
+
+                for (int i = 0; i < numerosHoja.Count; i++ )
+                {
+                    Camino c = new Camino(numerosHoja[i], tiposDeObjeto[i], identificadoresTramo[i], tiposCamino[i], longitud[i]);
+                    listCaminos.Add(c);
+                }
+
+                Camino.imprimir(listCaminos);
+            }
+        }
+    }
+}
